131ed394e88f1ccc9669548d314bd582621c3a6b
ravindersingh800 <rravindrasingh545@gmail.com>
2026-02-12 00:05:49 +0530

    Time: 0 ms (100%), Space: 8.4 MB (85.15%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,8 +1,21 @@
 class Solution {
 public:
     int mySqrt(int x) {
+        long left=0;
+        long right = x;
 
-        double square = floor(sqrt(x));
-        return square;
+        while(left<=right){
+            long mid= right+(left-right)/2;
+            if(mid*mid==x){
+                return mid;
+            }
+            else if(mid*mid>x){
+                right= mid-1;
+            }
+            else{
+                left= mid+1;
+            }
+        }
+        return right;
     }
 };
