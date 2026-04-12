bc2fc070a8ef94dfb18a178e000f0d5f07e47fdf
ravindersingh800 <rravindrasingh545@gmail.com>
2026-04-12 22:18:09 +0530

    Time: 0 ms (100%), Space: 8.2 MB (53.16%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string toLowerCase(string str) {
+        string result ="";
+        for(auto ch:str){
+            if(ch >='A' &&  ch <='Z'){
+                result+= ch+32;
+            }
+            else{
+                result+=ch;
+            }
+        }
+        return result;
+        
+    }
+};
